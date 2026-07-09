4f6e956cf7d52960714c39abc3e3dc3a821c9821
shravnik21 <kulkarnipshravni@gmail.com>
2026-07-09 20:09:20 +0530

    supabase_config modified

--- lib/config/supabase_config.dart
+++ lib/config/supabase_config.dart
@@ -7,7 +7,7 @@ class SupabaseConfig {
 
     await Supabase.initialize(
       url: dotenv.env['SUPABASE_URL']!,
-      anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+      publishableKey: dotenv.env['SUPABASE_ANON_KEY']!,
     );
   }
 

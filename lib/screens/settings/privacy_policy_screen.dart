3cd74599e851e7c84ee4bf1a9dedb30ab49ed80f
shravnik21 <kulkarnipshravni@gmail.com>
2026-07-19 00:02:16 +0530

    Update: lib/screens/settings/privacy_policy_screen.dart

--- lib/screens/settings/privacy_policy_screen.dart
+++ lib/screens/settings/privacy_policy_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'legal_document_screen.dart';
+import 'package:homely_app/screens/settings/legal_document_screen.dart';
 
 class PrivacyPolicyScreen extends StatelessWidget {
   const PrivacyPolicyScreen({super.key});

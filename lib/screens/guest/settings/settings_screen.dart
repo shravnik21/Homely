eb4ee26372fb7e874aa1deb00b2d243d05da46a2
shravnik21 <kulkarnipshravni@gmail.com>
2026-08-03 15:31:53 +0530

    Update: lib/screens/guest/settings/settings_screen.dart

--- lib/screens/guest/settings/settings_screen.dart
+++ lib/screens/guest/settings/settings_screen.dart
@@ -248,7 +248,7 @@ class _SettingsScreenState extends State<SettingsScreen> {
           Switch(
             value: value,
             onChanged: onChanged,
-            activeColor: AppColors.primary,
+            activeThumbColor: AppColors.primary,
           ),
         ],
       ),

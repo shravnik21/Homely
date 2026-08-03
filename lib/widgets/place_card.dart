14eef4128060aab7f2175b3fb220484b163d0fbe
shravnik21 <kulkarnipshravni@gmail.com>
2026-08-03 15:31:53 +0530

    Update: lib/widgets/place_card.dart

--- lib/widgets/place_card.dart
+++ lib/widgets/place_card.dart
@@ -32,7 +32,7 @@ class PlaceCard extends StatelessWidget {
           borderRadius: BorderRadius.circular(20),
           boxShadow: [
             BoxShadow(
-              color: Colors.black.withOpacity(0.06),
+              color: Colors.black.withValues(alpha: 0.06),
               blurRadius: 16,
               offset: const Offset(0, 6),
             ),
@@ -77,7 +77,7 @@ class PlaceCard extends StatelessWidget {
                         width: 36,
                         height: 36,
                         decoration: BoxDecoration(
-                          color: Colors.black.withOpacity(0.28),
+                          color: Colors.black.withValues(alpha: 0.28),
                           shape: BoxShape.circle,
                         ),
                         alignment: Alignment.center,

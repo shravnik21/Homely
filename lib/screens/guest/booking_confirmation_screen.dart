8adc604f7730843ce11ecf41ac3b3b671d87f46a
shravnik21 <kulkarnipshravni@gmail.com>
2026-08-03 15:31:53 +0530

    Update: lib/screens/guest/booking_confirmation_screen.dart

--- lib/screens/guest/booking_confirmation_screen.dart
+++ lib/screens/guest/booking_confirmation_screen.dart
@@ -136,7 +136,7 @@ class BookingConfirmationScreen extends StatelessWidget {
         borderRadius: BorderRadius.circular(20),
         boxShadow: [
           BoxShadow(
-            color: Colors.black.withOpacity(0.06),
+            color: Colors.black.withValues(alpha: 0.06),
             blurRadius: 16,
             offset: const Offset(0, 6),
           ),
@@ -315,7 +315,7 @@ class BookingConfirmationScreen extends StatelessWidget {
         color: AppColors.white,
         boxShadow: [
           BoxShadow(
-            color: Colors.black.withOpacity(0.08),
+            color: Colors.black.withValues(alpha: 0.08),
             blurRadius: 16,
             offset: const Offset(0, -4),
           ),

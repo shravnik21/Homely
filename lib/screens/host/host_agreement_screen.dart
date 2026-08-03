ce00e70f5921856329200d702cb6cbcbd1d5fbd8
shravnik21 <kulkarnipshravni@gmail.com>
2026-08-03 15:31:53 +0530

    Update: lib/screens/host/host_agreement_screen.dart

--- lib/screens/host/host_agreement_screen.dart
+++ lib/screens/host/host_agreement_screen.dart
@@ -106,7 +106,7 @@ class _HostAgreementScreenState extends State<HostAgreementScreen> {
                 color: AppColors.white,
                 boxShadow: [
                   BoxShadow(
-                    color: Colors.black.withOpacity(0.06),
+                    color: Colors.black.withValues(alpha: 0.06),
                     blurRadius: 12,
                     offset: const Offset(0, -3),
                   ),

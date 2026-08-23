7d2586c019bc9c7e6b72152e19cc32454678d64e
shravnik21 <kulkarnipshravni@gmail.com>
2026-08-23 23:19:15 +0530

    Update: lib/screens/splash_screen.dart

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:homely_app/config/app_theme.dart';
 import 'package:homely_app/services/auth_service.dart';
 import 'package:homely_app/screens/auth/login_screen.dart';
-import 'package:homely_app/screens/guest/home_screen.dart';
+import 'package:homely_app/screens/guest/guest_root_screen.dart';
 import 'package:homely_app/screens/host/host_home_screen.dart';
 import 'package:homely_app/screens/host/host_onboarding_screen.dart';
 
@@ -39,7 +39,7 @@ class _SplashScreenState extends State<SplashScreen> {
                 ? const HostOnboardingScreen()
                 : isHost
                     ? const HostHomeScreen()
-                    : const HomeScreen(),
+                    : const GuestRootScreen(),
       ),
     );
   }

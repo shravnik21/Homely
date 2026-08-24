3f7bd9c002c51c2ee93e8c87af0598375609575d
shravnik21 <kulkarnipshravni@gmail.com>
2026-08-24 16:07:21 +0530

    Update: lib/screens/auth/login_screen.dart

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -5,7 +5,7 @@ import 'package:homely_app/services/auth_service.dart';
 import 'package:homely_app/widgets/custom_textfield.dart';
 import 'package:homely_app/widgets/primary_button.dart';
 import 'package:homely_app/screens/guest/guest_root_screen.dart';
-import 'package:homely_app/screens/host/host_home_screen.dart';
+import 'package:homely_app/screens/host/host_root_screen.dart';
 import 'package:homely_app/screens/host/host_onboarding_screen.dart';
 import 'package:homely_app/screens/auth/signup_screen.dart';
 import 'package:homely_app/utils/network_error_helper.dart';
@@ -60,7 +60,7 @@ class _LoginScreenState extends State<LoginScreen> {
           builder: (_) => needsOnboarding
               ? const HostOnboardingScreen()
               : isHost
-                  ? const HostHomeScreen()
+                  ? const HostRootScreen()
                   : const GuestRootScreen(),
         ),
         (route) => false,
